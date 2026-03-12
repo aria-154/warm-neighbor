95623f3daac8f053b70172a6db285aae6a23a201
Developer <developer@example.com>
2026-03-12 23:37:07 +0800

    Update

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import uni from '@dcloudio/vite-plugin-uni'
+
+export default defineConfig({
+  plugins: [uni()],
+  server: {
+    port: 3000,
+    host: '0.0.0.0'
+  },
+  build: {
+    outDir: 'unpackage/dist/build/h5',
+    assetsDir: 'static',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {}
+      }
+    }
+  }
+})
